e00385832ab9edb5883ada94366d7241c563ed57
mary-soroka <49789721+mary-soroka@users.noreply.github.com>
2024-02-01 09:57:01 -0500

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 /* HIDE_ELEMENTS_ON_SCROLL_DOWN */
 
 $(window).scroll(function(){
-  $(".video-background").css("opacity", 1 - $(window).scrollTop() / 500);
-  $(".top_light").css("opacity", 1 - $(window).scrollTop() / 500);
-  $(".top_left_light").css("opacity", 1 - $(window).scrollTop() / 500);
+  $(".video-background").css("opacity", 1 - $(window).scrollTop() / 200);
+  $(".top_light").css("opacity", 1 - $(window).scrollTop() / 200);
+  $(".top_left_light").css("opacity", 1 - $(window).scrollTop() / 200);
 });
 
 /* SHOW_ELEMENTS_ON_SCROLL_DOWN  */
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 100) {
-    $('.top_dark').fadeIn(500);
-    $('.top_left_dark').fadeIn(500);
+  if (y > 200) {
+    $('.top_dark').fadeIn(200);
+    $('.top_left_dark').fadeIn(200);
+    $('.bottom_right').fadeIn(200);
   } else {
-    $('.top_dark').fadeOut(500);
-    $('.top_left_dark').fadeOut(500);
+    $('.top_dark').fadeOut();
+    $('.top_left_dark').fadeOut();
+    $('.bottom_right').fadeOut();
   }
 });
