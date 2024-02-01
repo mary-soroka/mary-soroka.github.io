d48a52830b9721d4ff0b32f97b572061d8d28471
mary-soroka <49789721+mary-soroka@users.noreply.github.com>
2024-02-01 09:48:58 -0500

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,20 @@
 /* HIDE_ELEMENTS_ON_SCROLL_DOWN */
 
 $(window).scroll(function(){
-  $(".header").css("opacity", 1 - $(window).scrollTop() / 200);
-  $(".video").css("opacity", 1 - $(window).scrollTop() / 400);
-
-  $(".nav_top_right").css("opacity", 1 - $(window).scrollTop() / 500);
-
-  $(".detail_text_top_left").css("opacity", 1 - $(window).scrollTop() / 100);
-  $(".detail_text_bottom_middle").css("opacity", 1 - $(window).scrollTop() / 50);
-
-  $(".detail_text_bottom_left").css("opacity", 1 - $(window).scrollTop() / 800);
-  $(".detail_text_bottom_right").css("opacity", 1 - $(window).scrollTop() / 800);
+  $(".video-background").css("opacity", 1 - $(window).scrollTop() / 500);
+  $(".top_light").css("opacity", 1 - $(window).scrollTop() / 500);
+  $(".top_left_light").css("opacity", 1 - $(window).scrollTop() / 500);
 });
 
-/* SHOW_ELEMENTS_ON_SCROLL_DOWN 
+/* SHOW_ELEMENTS_ON_SCROLL_DOWN  */
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
   if (y > 100) {
-    $('.back').fadeIn(500);
+    $('.top_dark').fadeIn(500);
+    $('.top_left_dark').fadeIn(500);
   } else {
-    $('.back').fadeOut();
+    $('.top_dark').fadeOut(500);
+    $('.top_left_dark').fadeOut(500);
   }
 });
-
-*/
