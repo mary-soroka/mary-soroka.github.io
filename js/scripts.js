8f01fe095cee972a6d30d0ef94a8729c37ae99f5
mary-soroka <49789721+mary-soroka@users.noreply.github.com>
2024-02-01 11:12:36 -0500

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(window).scroll(function(){
   $(".video-background").css("opacity", 1 - $(window).scrollTop() / 200);
   $(".top_right_light").css("opacity", 1 - $(window).scrollTop() / 200);
   $(".top_left_light").css("opacity", 1 - $(window).scrollTop() / 200);
+  $(".top_left_dark_mobile").css("opacity", 1 - $(window).scrollTop() / 200);
 });
 
 /* SHOW_ELEMENTS_ON_SCROLL_DOWN  */
