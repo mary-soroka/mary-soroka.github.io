579857f1943f41e7a5a5b853a0e576a43ab956d3
mary-soroka <49789721+mary-soroka@users.noreply.github.com>
2024-06-13 18:23:46 -0400

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 /* HIDE_ELEMENTS_ON_SCROLL_DOWN */
 
 $(window).scroll(function(){
-  $(".video-background").css("opacity", 1 - $(window).scrollTop() / 200);
-  $(".top_right_light").css("opacity", 1 - $(window).scrollTop() / 200);
-  $(".top_left_light").css("opacity", 1 - $(window).scrollTop() / 200);
-  $(".top_left_dark_mobile").css("opacity", 1 - $(window).scrollTop() / 200);
-  $(".bottom_left").css("opacity", 1 - $(window).scrollTop() / 200);
+  $(".video-background").css("opacity", 1 - $(window).scrollTop() / 100);
+  $(".top_right_light").css("opacity", 1 - $(window).scrollTop() / 100);
+  $(".top_left_light").css("opacity", 1 - $(window).scrollTop() / 100);
+  $(".top_left_dark_mobile").css("opacity", 1 - $(window).scrollTop() / 100);
+  $(".bottom_left").css("opacity", 1 - $(window).scrollTop() / 100);
 });
 
 /* SHOW_ELEMENTS_ON_SCROLL_DOWN  */
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 200) {
-    $('.bottom_right').fadeIn(200);
+  if (y > 100) {
+    $('.bottom_right').fadeIn(100);
   } else {
     $('.bottom_right').fadeOut();
   }
